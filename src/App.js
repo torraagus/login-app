1644f2625584e9f4f816d19ad1cbff0bc3db947e
Torra Agustin <torra.agustinignacio@gmail.com>
2020-04-17 15:59:48 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>This is hot reloading!
+                <h2>Hello world!</h2>
+            </div>
+        )
+    }
+}
